439036a831e5179331fa345c1518232073aaa465
Thomas Belin <tom@doyoubuzz.com>
2016-09-21 18:23:32 +0200

    Handle game states

--- src/application.js
+++ src/application.js
@@ -7,21 +7,45 @@ const ias = req.keys().map(function(key) {
     return req(key);
 });
 
+let gameState = {
+    paused: true,
+    fps: 1
+};
+
+document
+    .getElementById("play-button")
+    .addEventListener("click", function () {
+        gameState.paused = !gameState.paused
+        this.innerText = gameState.paused ? "▶" : "▮▮";
+    });
+
+document
+    .getElementById("inc-speed")
+    .addEventListener("click", () => gameState.fps += 5);
+
+document
+    .getElementById("dec-speed")
+    .addEventListener("click", () => gameState.fps -= 5);
+
 (function main() {
     let state = game.init(ias);
 
     renderDOM.init(state.mapSize);
+    renderDOM.render(state);
+
     function step() {
-        state = game.update(state);
-        if (typeof window !== "undefined") {
-            renderDOM.render(state);
-        } else {
-            renderConsole(state);
+        if (!gameState.paused) {
+            state = game.update(state);
+            if (typeof window !== "undefined") {
+                renderDOM.render(state);
+            } else {
+                renderConsole(state);
+            }
+            if (state.winner) {
+                return;
+            }
         }
-        if (state.winner) {
-            return;
-        }
-        setTimeout(step, 60);
+        setTimeout(step, 1000 / gameState.fps);
     }
 
     step();
